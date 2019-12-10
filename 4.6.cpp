c4bb1c55da588d9e809c0f00df9440e7d422f73e
CRASHFIRE-666 <54841369+CRASHFIRE-666@users.noreply.github.com>
2019-12-10 15:26:58 +0300

    Create 4.6.cpp

---
+++ 4.6.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    float a1,b1,c1,a2,b2,c2,x,y;
+    cout<<"Введите коэффиценты"<<endl;
+    cin>>a1>>b1>>c1>>a2>>b2>>c2;
+    x=((c1*b2)-(b1*c2))/((a1*b2)-(a2*b1));
+    y=((a1*c2)-(a2*c1))/((a1*b2)-(a2*b1));
+    cout<<"X="<<x<<", Y="<<y<<endl;
+}
