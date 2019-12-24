a3e50c86ccf66f788c2cfcc5357e991490cd74e4
CRASHFIRE666 <54841369+CRASHFIRE-666@users.noreply.github.com>
2019-12-24 12:40:03 +0300

    Update 4.6.cpp

--- 4.6.cpp
+++ 4.6.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
+#include <string> 
+#include <cmath>
 using namespace std;
-
 int main()
 {
-    setlocale(LC_ALL, "Russian");
-    float a1,b1,c1,a2,b2,c2,x,y;
-    cout<<"Введите коэффиценты"<<endl;
-    cin>>a1>>b1>>c1>>a2>>b2>>c2;
-    x=((c1*b2)-(b1*c2))/((a1*b2)-(a2*b1));
-    y=((a1*c2)-(a2*c1))/((a1*b2)-(a2*b1));
-    cout<<"X="<<x<<", Y="<<y<<endl;
+	double a1, b1, c1, a2, b2, c2, x, y, det, detx, dety;
+
+	cin >> a1 >> b1 >> c1 >> a2 >> b2 >> c2;
+
+	det =a1 * b2 - b1 * a2;
+	detx = c1 * b2 - b1 * c2;
+	dety = a1 * c2 - c1 * a2;
+	x = detx / det;
+	y = dety / det;
+	
+	cout << "x = " << x << endl;
+	cout << "y = " << y << endl;
+
+	
+	
+
+	system("pause");
+	return 0;
+
 }
