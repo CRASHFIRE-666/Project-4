3951d8f48b9c4d5f1cefddada39412561e76197e
CRASHFIRE666 <54841369+CRASHFIRE-666@users.noreply.github.com>
2019-12-24 12:36:39 +0300

    Update 4.3.cpp

--- 4.3.cpp
+++ 4.3.cpp
@@ -5,20 +5,21 @@ using namespace std;
 int main()
 {
     setlocale(LC_ALL, "Russian");
-	double price1, price2, amount1, amount2;
+    double price1, price2, amount1, amount2;
 
     cout << "Введите килограммы и цену";
-	cin >> amount1 >> price1;
 	
-	cout << "Введите килограммы";
-	cin >> amount2;
+    cin >> amount1 >> price1;
 	
-	price2 = price1 * amount2 / amount1;
+    cout << "Введите килограммы";
+    cin >> amount2;
 	
-	cout << "Новая цена:" << price2 << endl;
-	cout << "Цена 1 килограмма:" << price2 / amount2 << endl;
+    price2 = price1 * amount2 / amount1;
 	
-	system("pause");
-	return 0;
+    cout << "Новая цена:" << price2 << endl;
+    cout << "Цена 1 килограмма:" << price2 / amount2 << endl;
+	
+    system("pause");
+    return 0;
 
 }
