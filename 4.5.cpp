495abaca356aef7016b7d936e0b73d1955480f08
CRASHFIRE666 <54841369+CRASHFIRE-666@users.noreply.github.com>
2019-12-24 12:38:26 +0300

    Update 4.5.cpp

--- 4.5.cpp
+++ 4.5.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
+#include <string> 
+#include <cmath>
 using namespace std;
 int main()
 {
     setlocale(LC_ALL, "Russian");
-    float a,b,x;
-    cout<<"Введите коэффиценты"<<endl;
-    cin>>a>>b;
-    x=-b/a;
-    cout<<"X="<<x<<endl;
+	double a, b, x;
+
+	cin >> a >> b;
+
+	x = (-b) / a;
+
+	if (a != 0)
+	{
+		cout << x << endl;
+	}
+	else
+	{
+		cout << "НЕВЕРНОЕ ЗНАЧЕНИЕ" << endl;
+	}
+
+	
+	
+
+	system("pause");
+	return 0;
+
 }
