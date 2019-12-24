eb40046c9afe384ad7a3f572333cc05108d586e5
CRASHFIRE666 <54841369+CRASHFIRE-666@users.noreply.github.com>
2019-12-24 12:30:26 +0300

    Update 4.1.cpp

--- 4.1.cpp
+++ 4.1.cpp
@@ -3,14 +3,11 @@ using namespace std;
 
 int main()
 {
-    setlocale(LC_ALL, "Russian");
-    cout << "Введите число" << endl;
-    double a;
-cin >> a;
+	double a;
+	cin >> a;
 
     cout << a / 180 << endl;
 
     return 0;
 
 }
-   
